8c75058c59266ecaf0a27d1ac029faead32f33e9
sheb <abushebin@gmail.com>
2025-04-07 00:24:00 +0530

    mongo db connection set

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var hbs = require("express-handlebars");
+const expressFileUpload = require("express-fileupload");
+var db = require("./config/connection");
+console.log("✅ DB module loaded", db);
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var userRouter = require("./routes/user");
+var adminRouter = require("./routes/admin");
 const { extname } = require("path/posix");
 
 var app = express();
 
+try {
+  db.connect((err) => {
+    console.log("Starting app.js...", err);
+
+    if (err) console.log("console error");
+    else console.log("Database running successfully on port 27017");
+  });
+  console.log("db connecion established");
+} catch (e) {
+  console.log(e);
+}
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -29,9 +43,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(expressFileUpload());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", userRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/viewAllProducts.js
@@ -0,0 +1,3 @@
+router.get("/viewAllProducts", function (req, res) {
+  res.render("viewAllProducts");
+});
